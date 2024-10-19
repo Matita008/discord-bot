27781c6b01f0d48a98540017d76615750cf14c7c
Matita008 <155455608+Matita008@users.noreply.github.com>
2024-10-19 19:39:09 +0200

    added todo

--- src/main/java/org/matita08/listener/AdminSlashCommands.java
+++ src/main/java/org/matita08/listener/AdminSlashCommands.java
@@ -3,15 +3,15 @@ package org.matita08.listener;
 import net.dv8tion.jda.api.events.interaction.command.*;
 
 public class AdminSlashCommands {
-    public static void save(SlashCommandInteractionEvent event) {//TODO
+    public static void save(SlashCommandInteractionEvent event) {//TODO please implement me ;(
 
     }
 
-    public static void kill(SlashCommandInteractionEvent event) {//TODO
+    public static void kill(SlashCommandInteractionEvent event) {//TODO please implement me ;(
 
     }
 
-    public static void emergency(SlashCommandInteractionEvent event) {//TODO
+    public static void emergency(SlashCommandInteractionEvent event) {//TODO please implement me ;(
 
     }
 }
